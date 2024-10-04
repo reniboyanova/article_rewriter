4bca66c465bd033d4663d7a61a6d3db8c67dfe09
Reni Kostadinova <reni91boyanova@gmail.com>
2024-10-04 14:17:52 +0300

    Add parsers; Refactor article rewriter

--- artcle_rewriter/article_rewriter.py
+++ artcle_rewriter/article_rewriter.py
@@ -2,23 +2,43 @@ import torch
 import requests
 from openai import OpenAI
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
-from scraper import Scraper
+from parsers import HTMLScraper, BaseParser, HubberturkParser, RawData
 from utils.utils import load_json, get_api_key
 
 PROMPTS = load_json("./prompts.json").get('prompts', [])
 
+def part_prompt(client: OpenAI, part_prompt, max_len=4096):
+    chat_completion_text = client.chat.completions.create(
+        messages=[
+            {"role": "system", "content": "You are a skilled content writer who creates human-like articles."},
+            {"role": "user", "content": part_prompt}
+                ],
+            model="gpt-4o",
+            max_tokens=max_len,
+            temperature=0.8
+            )
+            
+    return chat_completion_text.choices[0].message.content.strip()
+
+
 class AIRewriter:
-    def __init__(self, src_url, prompts_list) -> None:
+    def __init__(self, src_url, parser: BaseParser, prompts_list) -> None:
         self.__scraper = None
+        self.__data: RawData = None
         
         try:
-            self.__scraper =  Scraper(src_url)
+            self.__scraper =  HTMLScraper(src_url)
         except Exception as e:
             raise Exception(f"Error from Scraper, self.__scraper can not be set of exception: {e}")
         
         if self.__scraper is not None:
-            self.__scraper.extract_data()
-            self.data = self.__scraper.data
+            soup = self.__scraper.make_soup()
+            if not isinstance(parser, BaseParser):
+                print(f"Parser need to be instanse of BaseParser, but got {type(parser)}")
+                self.__parser = BaseParser(soup)
+            else:
+                self.__parser = parser
+        self.__data = self.__parser.extract_raw_data()
         
         self.prompts = prompts_list
         if not self.prompts:
@@ -28,63 +48,20 @@ class AIRewriter:
         self.__api_key = get_api_key('./OPEN_AI_API_KET.txt')
             
         
-    def gpt_rewriter(self, max_len=4096) -> dict:
+    def gpt_rewriter(self) -> dict:
         client = OpenAI(api_key=self.__api_key)
-        
-        title = self.data.get('title', '')
-        anonse = self.data.get('anonse', '')
-        article_text = self.data.get('article_text', '')
-        
+
         results = {}
 
         for index, prompt in enumerate(self.prompts):
-            title_prompt = f"{prompt}\n\nRewrite the title:\n{title}"
-            anonse_prompt = f"{prompt}\n\nRewrite the subtitle (anonse):\n{anonse}"
-            text_prompt = f"{prompt}\n\nRewrite the article text:\n{article_text}"
+            title_prompt = f"{prompt}\n\nRewrite the title:\n{self.__data.title}"
+            anonse_prompt = f"{prompt}\n\nRewrite the subtitle (anonse):\n{self.__data.anonse}"
+            text_prompt = f"{prompt}\n\nRewrite the article text:\n{self.__data.text}"
 
-            # Request for the rewritten title
-            chat_completion_title = client.chat.completions.create(
-                messages=[
-                    {"role": "system", "content": "You are a skilled content writer who creates human-like articles."},
-                    {"role": "user", "content": title_prompt}
-                ],
-                model="gpt-4o",
-                max_tokens=max_len,
-                temperature=0.8
-            )
-            
-            rewritten_title = chat_completion_title.choices[0].message.content.strip()
-
-            # Request for the rewritten anonse
-            chat_completion_subtitle = client.chat.completions.create(
-                messages=[
-                    {"role": "system", "content": "You are a skilled content writer who creates human-like articles."},
-                    {"role": "user", "content": anonse_prompt}
-                ],
-                model="gpt-4o",
-                max_tokens=max_len,
-                temperature=0.8
-            )
-            
-            rewritten_anonse = chat_completion_subtitle.choices[0].message.content.strip()
-
-            # Request for the rewritten article text
-            chat_completion_text = client.chat.completions.create(
-                messages=[
-                    {"role": "system", "content": "You are a skilled content writer who creates human-like articles."},
-                    {"role": "user", "content": text_prompt}
-                ],
-                model="gpt-4o",
-                max_tokens=max_len,
-                temperature=0.8
-            )
-            
-            rewritten_article_text = chat_completion_text.choices[0].message.content.strip()
-            
             results[index] = {
-                'title': rewritten_title,
-                'anonse': rewritten_anonse,
-                'text': rewritten_article_text
+                'title': part_prompt(client, title_prompt),
+                'anonse': part_prompt(client, anonse_prompt),
+                'text': part_prompt(client, text_prompt)
             }
         
         return results
---
+++ artcle_rewriter/parsers.py
@@ -0,0 +1,73 @@
+import requests
+import json
+from requests.exceptions import HTTPError
+from bs4 import BeautifulSoup
+
+
+class HTMLScraper:
+    def __init__(self, source_url: str) -> None:
+        self.url = source_url
+        self.soup = None
+                
+    def get_response(self) -> str:
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            return response.text
+        except HTTPError as http_err:
+            print(f"HTTP error occurred: {http_err}") 
+        except Exception as err:
+            print(f"Other error occurred: {err}")
+    
+    def make_soup(self) -> BeautifulSoup:
+        if self.soup is None:
+            respons_text = self.get_response()
+            self.soup = BeautifulSoup(respons_text, 'html.parser')
+
+class RawData:
+    def __init__(self, title, anonse, text) -> None:
+        self.title = title
+        self.anonse = anonse
+        self.text = text
+        
+class BaseParser:
+    def __init__(self, soup: BeautifulSoup) -> None:
+        self._soup = soup
+
+    def extract_raw_data(self, title_tag="headline", anonse_tag="description", article_body_tag="p") -> RawData:
+        script_tag = self._soup.find('script', type='application/ld+json')
+        if script_tag:
+            try:
+                json_data = json.loads(script_tag.string)
+                title = json_data.get(title_tag, '')
+                anonse = json_data.get(anonse_tag, '')
+                try:
+                    paragraphs = self._soup.find_all(article_body_tag)
+                    article_text = ' '.join([p.get_text() for p in paragraphs])
+                except Exception as e:
+                    print(f"Exceptions while extracting article body: {e}")
+            except json.JSONDecodeError as e:
+                print(f"Error decoding json. Exception: {e}")
+        return RawData(title, anonse, article_text)
+
+
+class SoscuParser(BaseParser):
+    def __init__(self, soup: BeautifulSoup) -> None:
+        super().__init__(soup)
+
+        
+class HurriyetParser(BaseParser):
+    def __init__(self, soup: BeautifulSoup) -> None:
+        super().__init__(soup)
+
+
+class CumhuriyetParser(BaseParser):
+    def __init__(self, soup: BeautifulSoup) -> None:
+        super().__init__(soup)
+        
+
+class HubberturkParser(BaseParser):
+    def __init__(self, soup: BeautifulSoup) -> None:
+        super().__init__(soup)
+        
+   
